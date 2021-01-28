7d22a26ff3f99e13e8f4503519ee41abf3224200
r3tama <davidgalarzo@devaway.es>
2021-01-28 17:00:51 +0100

    First commit

--- app.py
+++ app.py
@@ -1,59 +1,40 @@
-import csv
-import os 
-from flask import Flask, render_template, redirect, url_for, request
+
+from flask import Flask, url_for, render_template, request, Response, redirect
 
 app = Flask(__name__)
 
- 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid Credentials, try again'
-		else:
-			return redirect(url_for('home'))
 
-	return render_template('login.html', error=error)	
-
-
-@app.route('/todo', methods=['POST', 'GET'])	#Right now it needs the file to exist for working
-												#And if you refresh without input it writes the last input it was declared
-def add_todo():
-	error = None
-	doneTodo = False
-	if os.path.getsize("todo.csv") > 0:
-		option = 'a'
-	else:
-		option = 'w'
-
-	with open('todo.csv', option, newline='') as file:
-		
-		writer = csv.writer(file)
-		if request.method == 'POST':
-			if request.form['submitButton'] == 'addtodo':
-				if len(request.form['todoItem']) > 0:
-					writer.writerow([request.form['todoItem']])
-				elif len(request.form['todoItem']) == 0:
-					error = "To do is empty"
-				elif request.form['submitButton'] == 'savetodo':
-					doneTodo = True
-					#Add something to close the file
-					
-				
-	
-	return render_template('todo.html', error=error)
+todoList = {
+    "Todo": [],
+}
 
 
 @app.route('/')
-def home():
-	return "Hello World"
+def index():
+    return render_template("index.html")
 
 
-@app.route('/welcome')
-def welcome():
-	return render_template('welcome.html')
+@app.route("/todoshow/", methods=['GET'])
+def show_todo_form():
+    return render_template("todo.html")
+
+
+@app.route("/todo/set/", methods=['POST'])
+def recover_data():
+    todoTask = request.form['todoInput']
+    if not checkIfEmpty(todoTask):
+        todoList["Todo"].append(todoTask)
+        return render_template("todo.html", data=todoList)
+    return render_template("todo.html", error="You need to enter a non empty todo task")
+
+
+@app.route("/todo/showAll/", methods=['GET'])
+def show_all_todos():
+    return render_template("allTodos.html", data=todoList)
+
+def checkIfEmpty(inputString):
+    return len(inputString) <= 0
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
