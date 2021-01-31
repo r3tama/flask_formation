a4bd65be1cb039c982c10b35b1fd855e78f98379
r3tama <davidgalarzo@devaway.es>
2021-01-31 17:39:03 +0100

    Fixed bug when crearting various to do lists

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, url_for, render_template, request, Response, redirect
 
 app = Flask(__name__)
 
+todoDict = {
 
-todoList = {
-    "Todo": [],
 }
+
+todoList = []
 
 
 @app.route('/')
@@ -21,19 +22,38 @@ def show_todo_form():
 
 @app.route("/todo/set/", methods=['POST'])
 def recover_data():
+    global todoList
     todoTask = request.form['todoInput']
-    if not checkIfEmpty(todoTask):
-        todoList["Todo"].append(todoTask)
-        return render_template("todo.html", data=todoList)
-    return render_template("todo.html", error="You need to enter a non empty todo task")
+    todoHeader = request.form['todoHeader'] 
+
+    if todoHeader in todoDict.keys():
+        todoList = todoDict[todoHeader] 
+        todoList.append(todoTask)
+    else: 
+        todoList.append(todoTask)
+        todoDict[todoHeader] = todoList
+    
+    todoList = []
+    return render_template("todo.html", data=todoDict) 
 
 
 @app.route("/todo/showAll/", methods=['GET'])
 def show_all_todos():
-    return render_template("allTodos.html", data=todoList)
+    return render_template("allTodos.html", data=todoDict)
+
+"""
+@app.route("/todo/show/<int:todo_id>/")
+def show_one_todo(todo_id=None):
+    return "The id is {}".format(todo_id)
+"""
+
 
 def checkIfEmpty(inputString):
     return len(inputString) <= 0
+
+
+def checkHeader(dict_, header):
+    return header in dict_.keys()
 
 
 if __name__ == '__main__':
