4d6e96a2742eca5757d2f321fd8f8eac2cbe7ea6
r3tama <davidgalarzo@devaway.es>
2021-02-01 13:42:54 +0100

    Deleted Frontend and implemented Delete correctly

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask, url_for, render_template, request, Response, redirect
+from flask import Flask, request, make_response
+
 app = Flask(__name__)
 todoDict = {}
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
-
-@app.route("/todoshow/", methods=['GET'])
-def show_todo_form():
-    return render_template("todo.html")
-
-
-@app.route("/todo/set/", methods=['POST'])
+@app.route("/todo/set/", methods=["POST"])
 def recover_data():
     todoTask = request.form['todoInput']
     todoHeader = request.form['todoHeader']
@@ -20,22 +12,28 @@ def recover_data():
         todoDict[todoHeader].append(todoTask)
     else:
         todoDict[todoHeader] = [todoTask]
-    return render_template("todo.html", data=todoDict)
+    return make_response(todoDict)
 
 
 @app.route("/todo/showAll/", methods=['GET'])
 def show_all_todos():
-    return render_template("allTodos.html", data=todoDict)
-
-#Delete function
+    return make_response(todoDict)
 
 
-def deleteHeader(dict_, header):
-    if header in dict_.keys():
-        dict_.pop(header)
+@app.route("/todo/delete/", methods=["DELETE"])
+def delete_todo():
+
+    if deleteHeader(todoDict, request.form['taskToDelete'],
+                    request.form['todoHeader']):
+        return make_response(todoDict, 204)
+    return make_response(todoDict, 404)
+
+
+def deleteHeader(dict_, taskToDelete, todoHeader):
+    if taskToDelete in dict_[todoHeader]:
+        dict_[todoHeader].remove(taskToDelete)
         return True
     return False
-
 
 
 if __name__ == '__main__':
