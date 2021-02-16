aa750e9c7938540df15da39db3b39b879ffef75c
r3tama <davidgalarzo@devaway.es>
2021-02-16 13:03:55 +0100

    Created templates with jinja

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template, redirect
 from shared.utils import deleteValueFromListInDict, existsKeyInDict, swapTaskFromListInDict
 app = Flask(__name__)
 todoDict = {}
 
 
-@app.route("/todo/set/", methods=["POST"])
+@app.route("/todo/set", methods=["POST"])
 def recover_data():
     if existsKeyInDict(request.form['todoHeader'], todoDict):
         todoDict[request.form['todoHeader']].append(request.form['todoInput'])
     else:
-        todoDict[request.form['todoHeader']] = [request.form['todoInput']]
-    return make_response(todoDict, 200)
+        if request.form['todoInput']:
+            todoDict[request.form['todoHeader']] = [request.form['todoInput']]
+    return redirect("/todo/showAll")
 
+@app.route("/", methods=["GET"])
+def base_template():
+    return render_template('base_template.html')
 
-@app.route("/todo/showAll/", methods=['GET'])
+@app.route("/fillFormRender", methods=["GET"])
+def render_form():
+    return render_template('form_input.html')
+
+@app.route("/todo/showAll", methods=['GET'])
 def show_all_todos():
-    return make_response(todoDict,200)
+    return render_template('show_dict.html',dict=todoDict)
 
 
-@app.route("/todo/delete/", methods=["DELETE"])
+@app.route("/todo/delete", methods=["DELETE"])
 def delete_todo():
     if deleteValueFromListInDict(request.form["todoHeader"], request.form["taskToDelete"], todoDict):
         return make_response(todoDict, 200)
     return make_response(todoDict, 404)
 
 
-@app.route("/todo/changeTask/", methods=['POST'])
+@app.route("/todo/changeTask", methods=['POST'])
 def change_task():
     if swapTaskFromListInDict(request.form["todoHeader"],request.form["swapouttask"],request.form["swapintask"],todoDict):
         return make_response(todoDict, 200)
